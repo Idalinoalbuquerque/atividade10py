a62df844343f4d7487b41e69c9f77e00bf49964e
Italoo220 <italosilava76@gmail.com>
2023-10-23 13:48:13 -0300

    first commit

---
+++ atividade10.py
@@ -0,0 +1,7 @@
+# Exercício Python 10: faça um algoritimo que receba a velocidade em Km/h de um veiculo e:
+# se maior que 60km/h aplicar multa de 7 vezes a da velocidade permitida
+velocidade = int(input("digite a velocidade em km/h: "))
+if velocidade> 60:
+    print("voçê foi multado! o limite é  60km/h")
+    multa = 60*7
+    print("a multa custara R${:.2f}".format(multa) )
